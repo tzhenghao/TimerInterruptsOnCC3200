4c17f5a917d3b71db9d95c0128a5fd445e850352
Zheng Hao Tan <tanzhao@umich.edu>
2015-11-22 16:23:06 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,226 @@
+//*****************************************************************************
+//
+// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
+// 
+// 
+//  Redistribution and use in source and binary forms, with or without 
+//  modification, are permitted provided that the following conditions 
+//  are met:
+//
+//    Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimer.
+//
+//    Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the   
+//    distribution.
+//
+//    Neither the name of Texas Instruments Incorporated nor the names of
+//    its contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//*****************************************************************************
+
+#include <stdlib.h>
+
+// Simplelink includes
+#include "simplelink.h"
+
+//Driverlib includes
+#include "hw_types.h"
+#include "hw_ints.h"
+#include "hw_memmap.h"
+#include "rom.h"
+#include "rom_map.h"
+#include "interrupt.h"
+#include "prcm.h"
+#include "timer.h"
+
+//Free_rtos/ti-rtos includes
+#include "osi.h"
+
+//Common interface includes
+#include "gpio_if.h"
+#include "common.h"
+#ifndef NOTERM
+#include "uart_if.h"
+#endif
+#include "pinmux.h"
+#include "timer_if.h"
+
+#include "timer_interrupt.h"
+
+#define SYS_CLK          80000000
+#define OSI_STACK_SIZE   3000
+
+extern void (* const g_pfnVectors[])(void);
+
+//****************************************************************************
+//                      LOCAL FUNCTION PROTOTYPES
+//****************************************************************************
+static void BoardInit(void);
+
+//****************************************************************************
+//                      FREERTOS FUNCTIONS
+//****************************************************************************
+
+//*****************************************************************************
+//
+//! Application defined hook (or callback) function - the tick hook.
+//! The tick interrupt can optionally call this
+//!
+//! \param  none
+//!
+//! \return none
+//!
+//*****************************************************************************
+void
+vApplicationTickHook( void )
+{
+}
+
+//*****************************************************************************
+//
+//! Application defined hook (or callback) function - assert
+//!
+//! \param  none
+//!
+//! \return none
+//!
+//*****************************************************************************
+void
+vAssertCalled( const char *pcFile, unsigned long ulLine )
+{
+    while(1)
+    {
+
+    }
+}
+
+//*****************************************************************************
+//
+//! Application defined idle task hook
+//!
+//! \param  none
+//!
+//! \return none
+//!
+//*****************************************************************************
+void
+vApplicationIdleHook( void )
+{
+
+}
+
+//*****************************************************************************
+//
+//! Application provided stack overflow hook function.
+//!
+//! \param  handle of the offending task
+//! \param  name  of the offending task
+//!
+//! \return none
+//!
+//*****************************************************************************
+void
+vApplicationStackOverflowHook( OsiTaskHandle *pxTask, signed char *pcTaskName)
+{
+    ( void ) pxTask;
+    ( void ) pcTaskName;
+
+    for( ;; );
+}
+
+void vApplicationMallocFailedHook()
+{
+    while(1)
+    {
+        // Infinite loop;
+    }
+}
+
+void InfiniteLoopTask(void *pvParameters)
+{
+    // GPIO Configuration
+    GPIO_IF_LedConfigure(LED1|LED3);
+
+    GPIO_IF_LedOff(MCU_RED_LED_GPIO);
+
+    TimerConfigNStart();
+
+    while(1);
+}
+
+//*****************************************************************************
+//
+//! Board Initialization & Configuration
+//!
+//! \param  None
+//!
+//! \return None
+//
+//*****************************************************************************
+static void BoardInit(void)
+{
+    // Set vector table base
+    MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
+
+    // Enable Processor
+    MAP_IntMasterEnable();
+    MAP_IntEnable(FAULT_SYSTICK);
+
+    PRCMCC3200MCUInit();
+}
+
+//****************************************************************************
+//! Main function
+//!
+//! \param none
+//! 
+//! \return None.
+//
+//****************************************************************************
+void main()
+{
+    // Initialize the board
+    BoardInit();
+    // Configure the pinmux settings for the peripherals exercised
+    PinMuxConfig();
+
+	#ifndef NOTERM
+		// Configuring UART
+		InitTerm();
+		ClearTerm();
+	#endif
+
+    // Start the TCPServer task
+    long lRetVal = osi_TaskCreate(InfiniteLoopTask,
+                    (const signed char *)"Infinite Loop Task",
+                    OSI_STACK_SIZE, 
+                    NULL, 
+                    1, 
+                    NULL );
+    if(lRetVal < 0)
+    {
+        ERR_PRINT(lRetVal);
+
+        while(1);
+    }
+
+    // Start the task scheduler
+    osi_start();
+
+    TimerDeinitStop();
+}
